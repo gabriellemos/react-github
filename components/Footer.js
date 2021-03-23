cab5fcf7e2437c4cdc2a24f7c9d26eb610c6ec43
Gabriel Lemos <gabrieldebritolemos@gmail.com>
2021-03-23 12:39:59 -0300

    Updated footer url to redirect to project source code

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ import styles from './Footer.module.scss'
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/gabriellemos/react-github" target="_blank" rel="noopener noreferrer">
         Source code at
         <img src="/GitHub_Logo.png" alt="Github Logo" />
       </a>
